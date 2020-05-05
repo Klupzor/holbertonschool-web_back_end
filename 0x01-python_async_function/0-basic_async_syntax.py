deff1e81bd8344abd52c87f398fd9a6dabcfb37a
vagrant <vagrant@vagrant.vm>
2020-05-05 21:36:07 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Python and Async"""
+import asyncio
+import time
+import random
+from typing import Optional
+
+
+async def wait_random(max_delay: Optional[int] = 10) -> float:
+    """returns delay value"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
