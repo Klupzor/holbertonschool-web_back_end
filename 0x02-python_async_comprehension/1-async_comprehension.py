b347e84909ba331684241ef6556cee5270b79893
vagrant <vagrant@vagrant.vm>
2020-05-06 17:17:22 +0000

    changing for generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Python async comprehension"""
-from typing import List
+from typing import Generator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> Generator[float, None, None]:
     """return azync list"""
     done_tasks = [i async for i in async_generator()]
     return done_tasks
