1aad1698c8313602965f1527a91d546d6ae3da90
vagrant <vagrant@vagrant.vm>
2020-05-13 23:21:44 +0000

    adding typing

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Pagination
 """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> [int, int]:
     """Return a tuple of size two containing a start index and an end index
     """
     return ((page - 1) * page_size, page * page_size)
