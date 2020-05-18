01c064f5163cb2ebed7da189f80cf81a7e95ecec
vagrant <vagrant@vagrant.vm>
2020-05-18 21:35:05 +0000

    task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,7 +2,25 @@
 """ Personal data
 """
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
