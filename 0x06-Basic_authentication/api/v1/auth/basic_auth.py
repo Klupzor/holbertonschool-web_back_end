e32b4b1d5134f9cc4db6b48b977641ce25f8f3f7
vagrant <vagrant@vagrant.vm>
2020-05-22 23:17:23 +0000

    task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,14 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ auth method
-        returns authorization header
+    """ class to manage the API authentication.
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header
+        """
+        if (authorization_header and
+            type(authorization_header) == str and
+                authorization_header.startswith("Basic ")):
+            return "".join(authorization_header.split(" ")[1:])
