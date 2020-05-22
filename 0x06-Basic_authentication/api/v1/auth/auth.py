47cea6cfa9970d2f28a04f9b42bbfe06c4c55901
vagrant <vagrant@vagrant.vm>
2020-05-22 22:07:14 +0000

    before request

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -23,8 +23,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ auth method
+        returns authorization header
         """
-        return None
+        if not request:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ auth method
