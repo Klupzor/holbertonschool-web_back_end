98bcb738490f895ed041e9603169f045b1e73ff8
vagrant <vagrant@vagrant.vm>
2020-05-22 20:51:47 +0000

    fixing requirements

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ auth method
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ auth method
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ auth method
+        """
         return None
