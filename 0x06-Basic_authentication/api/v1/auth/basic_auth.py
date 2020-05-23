8b8b28a280600e731744674300c3d5c2945c229f
vagrant <vagrant@vagrant.vm>
2020-05-23 15:09:32 +0000

    fixing key error

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,8 +46,9 @@ class BasicAuth(Auth):
         """
         if (user_email and user_pwd and
                 type(user_email) == str and type(user_pwd) == str):
-            users = User.search({"email": user_email})
-            if not users:
+            try:
+                users = User.search({"email": user_email})
+            except KeyError:
                 return None
             for user in users:
                 if user and user.is_valid_password(user_pwd):
