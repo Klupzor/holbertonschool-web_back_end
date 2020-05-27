ff3ce826ecaf5a429b3fe97603c4d66f36d2325a
vagrant <vagrant@vagrant.vm>
2020-05-27 22:00:58 +0000

    task 1

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ class to manage the API authentication.
+    """
+    pass
