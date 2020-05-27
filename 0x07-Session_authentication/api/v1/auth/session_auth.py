ce09892bc43157d077c53712ba38de75a9f9f119
vagrant <vagrant@vagrant.vm>
2020-05-27 23:59:41 +0000

    task 6

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@
 
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -26,3 +28,13 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) != str:
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns a User instance based on a cookie value
+        """
+        if not request:
+            return None
+        session_cookie = self.session_cookie(request)
+        if session_cookie:
+            user_id = self.user_id_for_session_id(session_cookie)
+            return User.get(user_id)
