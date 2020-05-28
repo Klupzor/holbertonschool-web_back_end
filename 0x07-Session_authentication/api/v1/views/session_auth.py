7e26ebf61165158fad99e4a6f90cb51310d17dc3
vagrant <vagrant@vagrant.vm>
2020-05-28 02:02:26 +0000

    fixing imports

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -4,6 +4,8 @@
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from api.v1.app import auth
+from models.user import User
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -16,16 +18,15 @@ def login():
         return jsonify(error="email missing"), 400
     if not pwd:
         return jsonify(error="password missing"), 400
-    try:
-        users = User.search({"email": email})
-        print(users)
-        for user in users:
-            if user.is_valid_password(pwd):
-                session = auth.create_session(user.id)
-                json = jsonify(user.to_json())
-                json.set_cookie(getenv('SESSION_NAME'), session)
-                return json
-            else:
-                return jsonify(error="wrong password"), 401
-    except Exception:
+    users = User.search({"email": email})
+    if not users:
         return jsonify(error="no user found for this email"), 404
+
+    for user in users:
+        if user.is_valid_password(pwd):
+            session = auth.create_session(user.id)
+            json = jsonify(user.to_json())
+            json.set_cookie(getenv('SESSION_NAME'), session)
+            return json
+        else:
+            return jsonify(error="wrong password"), 401
