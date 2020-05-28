fb2d1dad5aef4a14fcf8f3561caee262aa1138c7
vagrant <vagrant@vagrant.vm>
2020-05-28 00:27:03 +0000

    task 7

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module of session auth views
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """  Handles all routes for the Session authentication
+    """
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    if not email:
+        return jsonify(error="email missing"), 400
+    if not pwd:
+        return jsonify(error="password missing"), 400
+    try:
+        users = User.search({"email": email})
+        print(users)
+        for user in users:
+            if user.is_valid_password(pwd):
+                session = auth.create_session(user.id)
+                json = jsonify(user.to_json())
+                json.set_cookie(getenv('SESSION_NAME'), session)
+                return json
+            else:
+                return jsonify(error="wrong password"), 401
+    except Exception:
+        return jsonify(error="no user found for this email"), 404
