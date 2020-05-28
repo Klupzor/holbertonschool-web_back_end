bd0e34d680a43b69647acea1c51550de1405fbe4
vagrant <vagrant@vagrant.vm>
2020-05-28 02:31:16 +0000

    fixing path error

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -38,3 +38,17 @@ class SessionAuth(Auth):
         if session_cookie:
             user_id = self.user_id_for_session_id(session_cookie)
             return User.get(user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """ Deletes the user session / logout
+        """
+        if not request:
+            return False
+        session_cookie = self.session_cookie(request)
+        if not session_cookie:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if not user_id:
+            return False
+        self.user_id_by_session_id.pop(session_cookie)
+        return True
