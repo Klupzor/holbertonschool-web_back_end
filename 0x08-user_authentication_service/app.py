cf28fe669a130c5f195ddb6b4be10192ff93eabb
vagrant <vagrant@vagrant.vm>
2020-06-03 22:54:55 +0000

    task 7

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,11 @@
 """ Hash password in database
 """
 from flask import Flask, jsonify
+from auth import Auth
+
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -10,6 +14,21 @@ def index() -> str:
     """ Index page
     """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ post new user in /users
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({
+            "email": email, "message": "user created"
+        }), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
