70c9c01f5e89b9e4169d17bbf49196e57d475234
vagrant <vagrant@vagrant.vm>
2020-06-03 22:22:21 +0000

    task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Hash password
 """
+from db import DB
 import bcrypt
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -10,3 +12,24 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Auth class constructor
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register user
+        """
+        if email and password:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
+            else:
+                new_user = self._db.add_user(email, _hash_password(password))
+                return new_user
