8de301b8c88cd5721be352690e0e206d2eb4aaec
vagrant <vagrant@vagrant.vm>
2020-06-03 21:42:34 +0000

    Updating type

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -46,7 +46,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """ Update user
         """
         user = self.find_user_by(id=user_id)
