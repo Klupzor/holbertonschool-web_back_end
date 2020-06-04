fd40720a3b4341f7cbf7bc4373bafb011aa8ce1f
vagrant <vagrant@vagrant.vm>
2020-06-04 03:25:55 +0000

    destroy session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,3 +72,9 @@ class Auth:
                 return self._db.find_user_by(session_id=session_id)
             except NoResultFound:
                 return
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy session
+        """
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
