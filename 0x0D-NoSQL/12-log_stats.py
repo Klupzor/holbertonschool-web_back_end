4e2f54409e4ffd4cb42616aef4b33de13bd97ee1
vagrant <vagrant@vagrant.vm>
2020-06-23 19:11:04 +0000

    task 12

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" NoSQL Pymongo
+"""
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+data = client.logs.nginx
+logs = data.count_documents({})
+get = data.count_documents({"method": "GET"})
+post = data.count_documents({"method": "POST"})
+put = data.count_documents({"method": "PUT"})
+patch = data.count_documents({"method": "PATCH"})
+delete = data.count_documents({"method": "DELETE"})
+status = data.count_documents({"method": "GET", "path": "/status"})
+
+message = f"{logs} logs\n\
+Methods:\n\
+    method POST: {post}\n\
+    method PUT: {put}\n\
+    method GET: {get}\n\
+    method PATCH: {patch}\n\
+    method DELETE: {delete}\n\
+    {status} status check\
+"
+
+print(message)
