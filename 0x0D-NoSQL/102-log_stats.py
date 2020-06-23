6cc68febed5eb58e7b9cc779eb462c162719ef90
vagrant <vagrant@vagrant.vm>
2020-06-23 22:49:25 +0000

    task 102

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" NoSQL Pymongo
+"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ script that provides some stats about Nginx logs stored in MongoDB:
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    data = client.logs.nginx
+    logs = data.count_documents({})
+    get = data.count_documents({"method": "GET"})
+    post = data.count_documents({"method": "POST"})
+    put = data.count_documents({"method": "PUT"})
+    patch = data.count_documents({"method": "PATCH"})
+    delete = data.count_documents({"method": "DELETE"})
+    status = data.count_documents({"method": "GET", "path": "/status"})
+
+    message = f"\
+{logs} logs\n\
+Methods:\n\
+\tmethod GET: {get}\n\
+\tmethod POST: {post}\n\
+\tmethod PUT: {put}\n\
+\tmethod PATCH: {patch}\n\
+\tmethod DELETE: {delete}\n\
+{status} status check\
+"
+    print(message)
+    top_ips = data.aggregate([{
+        "$group": {"_id": "$ip", "myCount": {"$sum": 1}}},
+        {"$sort": {"myCount": -1}},
+        {"$limit": 10}
+    ])
+
+    print("IPs:")
+    for top in top_ips:
+        print(f"\t{top.get('_id')}: {top.get('myCount')}")
+
+
+if __name__ == "__main__":
+    log_stats()
