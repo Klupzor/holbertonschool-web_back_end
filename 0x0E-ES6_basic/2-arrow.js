0f7f7cac0060418031332b2086fcd245b8b73a7b
vagrant <vagrant@vagrant.vm>
2020-06-26 04:13:35 +0000

    task 2

---
+++ 0x0E-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+var getNeighborhoodsList = () => {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const self = this;
+    this.addNeighborhood = (newNeighborhood)  => {
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return self.sanFranciscoNeighborhoods;
+    };
+}
+
+module.exports = getNeighborhoodsList;
---
+++ 0x0E-ES6_basic/2-arrow.test.js
@@ -0,0 +1,7 @@
+const getNeighborhoodsList = require("./2-arrow");
+
+require("./2-arrow")
+
+test('add arrow', () => {
+  expect(true).toStrictEqual(true);
+});
