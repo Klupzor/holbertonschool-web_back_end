8add6dc108a79680d3fe540ef064d646124e4637
vagrant <vagrant@vagrant.vm>
2020-07-02 03:59:06 +0000

    fixing bugs

--- 0x0F-ES6_promise/6-all-reject.js
+++ 0x0F-ES6_promise/6-all-reject.js
@@ -2,16 +2,16 @@ const signUpUser = require('./4-all-reject');
 const uploadPhoto = require('./5-all-reject');
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const data = [];
   const user = await signUpUser(firstName, lastName);
   try {
+    data.push({ status: 'fulfilled', value: user });
     await uploadPhoto(fileName);
   } catch (err) {
-    if (err) {
-      return {
-        status: 'rejected',
-        value: `Error: ${fileName} cannot be processed`,
-      };
-    }
+    data.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
   }
-  return { status: 'fulfilled', value: user };
+  return data;
 }
