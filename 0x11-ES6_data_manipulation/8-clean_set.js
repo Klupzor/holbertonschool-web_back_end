ae3f1a6eb44991892e5d051770741b67bac4a3db
vagrant <vagrant@vagrant.vm>
2020-07-08 23:11:17 +0000

    Fixing conditions

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, str) {
+  if (!str || !str.length) return '';
   let val = '';
   for (const el of set) {
-    if (el.startsWith(str)) {
+    if (el && el.startsWith(str)) {
       val += val.length === 0 ? el.replace(str, '') : el.replace(str, '-');
     }
   }
