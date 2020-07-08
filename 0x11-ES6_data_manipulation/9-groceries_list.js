50a1b2df161a1f58b55cc47eea63316bd2a17072
vagrant <vagrant@vagrant.vm>
2020-07-08 23:48:16 +0000

    task 9

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const groceires = new Map();
+  groceires.set('Apples', 10);
+  groceires.set('Tomatoes', 10);
+  groceires.set('Pasta', 1);
+  groceires.set('Rice', 1);
+  groceires.set('Banana', 5);
+  return groceires;
+}
