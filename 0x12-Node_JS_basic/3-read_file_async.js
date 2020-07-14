d88bacb9f600069430ea0fd2677b88adafc1ff72
klupzor <klupzor@gmail.com>
2020-07-14 14:02:27 +0000

    task 3

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function csvJSON(csv) {
+  const lines = csv.split('\n');
+
+  const result = [];
+  const headers = lines[0].split(',');
+
+  for (let i = 1; i < lines.length; i += 1) {
+    const obj = {};
+    const currentline = lines[i].split(',');
+
+    for (let j = 0; j < headers.length; j += 1) {
+      obj[headers[j]] = currentline[j];
+    }
+
+    result.push(obj);
+  }
+  return result;
+}
+
+module.exports = async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const csv = csvJSON(data);
+    console.log(`Number of students: ${csv.length}`);
+    const cs = [];
+    const swe = [];
+    csv.forEach((item) => {
+      if (item.field === 'CS') {
+        cs.push(item.firstname);
+      } else {
+        swe.push(item.firstname);
+      }
+    });
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
