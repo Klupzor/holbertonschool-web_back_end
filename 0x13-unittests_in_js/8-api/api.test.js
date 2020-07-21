7ddf67b7770c4ce40b292a17ae3e6da0211fd282
klupzor <klupzor@gmail.com>
2020-07-21 04:20:26 +0000

    task 8

---
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,12 @@
+  
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Testing api', function() {
+    it('request GET', async function() {
+      await request({ url: 'http://localhost:7865', method: 'GET' }, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+      });
+    });
+});
