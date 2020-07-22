e42f863528c027cc337f5972929854db0c952350
klupzor <klupzor@gmail.com>
2020-07-22 03:02:48 +0000

    changing to done method

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,12 +2,13 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Testing api', async function() {
-    await it('request GET', function() {
-      request({ url: 'http://localhost:7865', method: 'GET' }, function(err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome to the payment system');
-        expect(body).to.be.a('string');
-      });
+describe('Testing api', function() {
+    it('request GET', function (done) {
+    request({ url: 'http://localhost:7865', method: 'GET' }, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      expect(body).to.be.a('string');
     });
+    done()
+  });
 });
