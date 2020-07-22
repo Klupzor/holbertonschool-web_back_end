bbddb33bb5a9698945d083f1daa2db988a55ff23
klupzor <klupzor@gmail.com>
2020-07-22 02:49:39 +0000

    fixing async

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,9 +2,9 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Testing api', function() {
-    it('request GET', async function() {
-      await request({ url: 'http://localhost:7865', method: 'GET' }, function(err, res, body) {
+describe('Testing api', async function() {
+    await it('request GET', function() {
+      request({ url: 'http://localhost:7865', method: 'GET' }, function(err, res, body) {
         expect(res.statusCode).to.equal(200);
         expect(body).to.equal('Welcome to the payment system');
       });
